f433cbb57ee649f5ecdb044dc5df4e51a2ecd6b8
aniskarout1 <aniskarout143@gmail.com>
2023-11-24 10:44:24 +0530

    ALL JAVASCRIPT FILES

---
+++ ar.js
@@ -0,0 +1,9 @@
+var i,j;
+for(i=5; i>0;i--)
+{
+    for(j=5; j>0;j--)
+    {
+        document.write(i);
+        // <br></br>
+    }
+}
---
+++ j.js
@@ -0,0 +1,8 @@
+var i,j;
+if(i=0;i<5;i++)
+{
+    if(j=0;j<5;j++)
+    {
+        document.write(i);
+    }
+}
